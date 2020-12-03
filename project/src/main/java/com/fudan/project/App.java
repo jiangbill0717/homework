25861dbd25c9b2c22700dd672f4054ad1b592c35
chengweili <chengweili@localhost>
2020-12-03 16:19:29 +0800

    modified

--- project/src/main/java/com/fudan/project/App.java
+++ project/src/main/java/com/fudan/project/App.java
@@ -1,13 +1,12 @@
 package com.fudan.project;
 
+import org.springframework.boot.SpringApplication;
+
 /**
- * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+	}
 }
